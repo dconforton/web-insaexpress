df147e4bd61f00a82106390745da23e99616e9fd
Lea Juskiewenski <lea.jski@gmail.com>
2019-05-13 19:43:42 +0200

    versionLeaLukas

--- angIE/src/app/app-routing.module.ts
+++ angIE/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NewComponent } from './new/new.component';
+import { UploadphotosComponent } from './uploadphotos/uploadphotos.component';
 
 const routes: Routes = [
-
+	{ path: '', component: NewComponent },
+	{ path: 'photos', component: UploadphotosComponent }
 ];
 
 @NgModule({
